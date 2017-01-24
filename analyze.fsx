8e62f8ed05976ec79fc61eb9ffb1d33e0ee447a1
Martin Jul <martin@mjul.com>
2017-01-24 15:25:57 +0100

    Extract ActivityLogParser to separate file

--- analyze.fsx
+++ analyze.fsx
@@ -1,57 +1,12 @@
 #r "./packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+#load "ActivityLogParser.fs"
 
+open ActivityLogParser
 open System
-open FSharp.Data
 
 
 // Set this to something that fits or specify the file on the command line
 let DEFAULT_FILENAME = "2016-12-13 full logfile.csv"
-
-type ActivityLogCsv = CsvProvider<"activity-log-template.csv">
-type FileDescriptorJson = JsonProvider<"activity-log-file-description-template.json">
-
-type Activity =
-    | FileAdded of path : string 
-    | FileEdited of path : string
-type User = | User of name : string * email : string
-type Event = {Time: DateTime; Activity: Activity; User: User}
-    
-
-let parseFileRow (row:ActivityLogCsv.Row) =
-    let info = FileDescriptorJson.Parse(row.Info)
-    match row.``Event type`` with
-    | "Edited files" ->
-        Some (FileEdited info.Path)
-    | "Added files" ->
-        Some (FileAdded info.Path)
-    | _ ->
-        None
-        
-let parseRow (row:ActivityLogCsv.Row) =
-    let user = User (row.Name, row.Email)
-    let activity =
-        match row.``Event category`` with
-        | "Files" ->
-            parseFileRow row
-        | _ ->
-            None
-    match activity with
-    | Some a ->
-        Some {Time=row.Time; Activity=a; User=user}
-    | None ->
-        None
-
-(* Parse the log and return the rows that we can parse *)
-let parseRows rows =
-    rows
-    |> Seq.map parseRow
-    |> Seq.choose id
-
-
-let parseActivityLog (fname:string) =
-    let rows = ActivityLogCsv.Load(fname).Rows
-    parseRows rows
-
 
 (* Create some reports *)
 
